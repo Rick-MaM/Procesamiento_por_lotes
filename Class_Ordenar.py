cf8a71ad850b8448147236b53cbfd7e0d3656513
Ricardo_Home <ricardo.macias1755@alumnos.udg.mx>
2023-02-18 01:30:22 -0600

    Se actualizo la funcion Separete

--- Class_Ordenar.py
+++ Class_Ordenar.py
@@ -4,13 +4,28 @@ class Ordenar():
     def __init__(self):
         self.name = []
 
+    #Eliminar la basura del archivo    
     def Separete(self,Line):
         self.name = Line.split(",")
+        self.name.pop(4)
+        self.name.pop(3)
+        self.name.pop(1)
+        
+        band = False
+        aux = ""
+        for row in self.name[0]:
+            if (row == "/"):
+                band = True
+            else:
+                if (band):
+                    pass
+                else:
+                    aux = aux + row
 
-
-
-
+        self.name[0] = aux
         
     
     def get_line(self):
         return self.name
+
+
