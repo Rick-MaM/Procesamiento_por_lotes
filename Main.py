dc65ae90848f37eb7859e2f196e48555c57b0639
Ricardo_Home <ricardo.macias1755@alumnos.udg.mx>
2023-02-18 20:39:49 -0600

    Se agrego la funcion main

--- Main.py
+++ Main.py
@@ -1,12 +1,49 @@
 import Clase_File
 import Class_Ordenar
+import os
 
 
-file = Clase_File.File()
+def main():
+    file = Clase_File.File("Este.txt")
+    
+
+    size = file.size()
+    
+    for i in range(size):
+        order = Class_Ordenar.Ordenar()
+        line = file.Read()
+        order.Separete(line[i])
+        order.Hexadecimal_Decimal()
+        order.Decimal_Hexadecimal()
+        order.Clean_Ipv4()
+        Date = order.Last_Name + order.IPv6 + order.IPv4 + "\n"
+        print(i)
+        
+        if os.path.exists("Este.txt"):
+            file.Edit(Date)
+        else:
+            file.create(Date)
+
+    return 
+
+main()
+"""
+file = Clase_File.File("Este.txt")
+line = file.Read()
 order = Class_Ordenar.Ordenar()
 
-line = file.Read()
+
+# Clase Ordenar
 result = order.Separete(line[0])
-print(result)
+order.Hexadecimal_Decimal()
+order.Decimal_Hexadecimal()
+order.Clean_Ipv4()
+Date = order.Last_Name + order.IPv6 + order.IPv4 + "\n"
+
+
+
+#Escribir
+file.Edit(Date)
+"""
 
 
