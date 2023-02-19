4026ca4584ee86a5d3fe5f10f2f99bbd064087be
Ricardo_Home <ricardo.macias1755@alumnos.udg.mx>
2023-02-18 21:45:55 -0600

    Se agrego una barra de cargar con tqdm

--- Main.py
+++ Main.py
@@ -1,25 +1,25 @@
 import Clase_File
 import Class_Ordenar
 import os
+from tqdm.auto import tqdm 
 
+name = "prueba2.txt"
 
 def main():
-    file = Clase_File.File("Este.txt")
-    
+    file = Clase_File.File(name)
 
     size = file.size()
     
-    for i in range(size):
-        order = Class_Ordenar.Ordenar()
+    for i in tqdm(range(size)):
         line = file.Read()
-        order.Separete(line[i])
+        order = Class_Ordenar.Ordenar(line[i])
+        order.Clean_IPv6()
         order.Hexadecimal_Decimal()
         order.Decimal_Hexadecimal()
         order.Clean_Ipv4()
         Date = order.Last_Name + order.IPv6 + order.IPv4 + "\n"
-        print(i)
         
-        if os.path.exists("Este.txt"):
+        if os.path.exists(name):
             file.Edit(Date)
         else:
             file.create(Date)
@@ -27,23 +27,3 @@ def main():
     return 
 
 main()
-"""
-file = Clase_File.File("Este.txt")
-line = file.Read()
-order = Class_Ordenar.Ordenar()
-
-
-# Clase Ordenar
-result = order.Separete(line[0])
-order.Hexadecimal_Decimal()
-order.Decimal_Hexadecimal()
-order.Clean_Ipv4()
-Date = order.Last_Name + order.IPv6 + order.IPv4 + "\n"
-
-
-
-#Escribir
-file.Edit(Date)
-"""
-
-
